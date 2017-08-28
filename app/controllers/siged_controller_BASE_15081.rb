95ea99edb4205117f7cc5ffe8361c4bbbd64c3b8
donnet <Donnet04@gmail.com>
2017-08-28 14:19:16 -0500

    merge conflicts solucionados

---
+++ app/controllers/siged_controller_BASE_15081.rb
@@ -0,0 +1,38 @@
+class SigedController < ApplicationController
+  after_action :authenticate_usuario!
+    def index
+        if usuario_signed_in?
+            if current_usuario.rol == 'admin'
+                render template: 'siged/menuAdmin'
+            elsif current_usuario.rol == "docente"
+               doc = Docente.find_by(email: current_usuario.email)
+             if  doc.usuario_id == nil
+                   redirect_to edit_docente_path(doc)
+             else
+                   render template: "siged/menuDocente"
+             end
+            elsif current_usuario.rol == "estudiante"
+                x = Estudiante.find_by(email: current_usuario.email)
+                if x.usuario_id == nil                    
+                    redirect_to edit_estudiante_path(x)
+                else
+                    render template: 'siged/menuEstudiante'
+                end
+<<<<<<< HEAD
+            end
+        end      
+    end
+end
+=======
+            elsif current_usuario.rol == "empresa"
+              emp = Empresa.find_by(email: current_usuario.email)
+              if  emp.usuario_id == nil
+                # redirect_to edit_empresa_path(emp)
+              elsif
+                render template: 'siged/menuEmpresa'
+              end
+            end
+        end      
+    end
+end
+>>>>>>> d937eca8b026c1b24d3e9d099d373ac3003575ae
---
+++ app/controllers/siged_controller_REMOTE_14401.rb
@@ -0,0 +1,32 @@
+class SigedController < ApplicationController
+  after_action :authenticate_usuario!
+    def index
+        if usuario_signed_in?
+            if current_usuario.rol == 'admin'
+                render template: 'siged/menuAdmin'
+            elsif current_usuario.rol == "docente"
+               doc = Docente.find_by(email: current_usuario.email)
+             if  doc.usuario_id == nil
+                   redirect_to edit_docente_path(doc)
+             else
+                   render template: "siged/menuDocente"
+             end
+            elsif current_usuario.rol == "estudiante"
+                x = Estudiante.find_by(email: current_usuario.email)
+                if x.usuario_id == nil                    
+                    redirect_to edit_estudiante_path(x)
+                else
+                    render template: 'siged/menuEstudiante'
+                end
+
+            elsif current_usuario.rol == "empresa"
+              emp = Empresa.find_by(email: current_usuario.email)
+              if  emp.usuario_id == nil
+                # redirect_to edit_empresa_path(emp)
+              elsif
+                render template: 'siged/menuEmpresa'
+              end
+            end
+        end      
+    end
+end
